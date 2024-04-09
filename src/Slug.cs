9c2e26baa510c2a29d53326a9c9ab6b4b46ece03
Johnny Kristian Moeberg Sørensen <mojo11ku@hotmail.com>
2024-04-09 14:53:27 -0100

    feat: Max cutoff slug

--- src/Slug.cs
+++ src/Slug.cs
@@ -15,21 +15,23 @@ public static class Slug
     /// <param name="input">The text input</param>
     /// <param name="connector">The connector character</param>
     /// <param name="separator">The separator character</param>
+    /// <param name="max">The maximum string length. The number of characters in the resulting string</param>
     /// <returns>A slug</returns>
-    public static string Create(string input, char connector = '_', char separator = '-')
+    public static string Create(string input, char connector = '_', char separator = '-', int? max = null)
     {
         var transliterate = input.Transliterate();
-        return ToLowercaseAndHyphenSeparator(transliterate, connector, separator);
+        return ToLowercaseAndHyphenSeparator(transliterate, connector, separator, max);
     }
 
-    private static string ToLowercaseAndHyphenSeparator(string text, char connector, char separator)
+    private static string ToLowercaseAndHyphenSeparator(string text, char connector, char separator, int? max)
     {
         Span<char> span = text.Length < 1000
             ? stackalloc char[text.Length]
             : new char[text.Length];
 
+        var limit = (max ?? text.Length) + 1;
         var j = 0;
-        for (var i = 0; i < text.Length; i++)
+        for (var i = 0; i < limit && i < text.Length; i++)
         {
             var c = text[i];
             var info = CharUnicodeInfo.GetUnicodeCategory(c);
