9b1ca8c1a643ac7d546a8c063573ab36184a0719
Newex <johnnykristianmoebergsoerensen@gmail.com>
2024-10-04 20:26:48 -0100

    test-red: Space with a number ending

--- tests/Testing.cs
+++ tests/Testing.cs
@@ -132,4 +132,17 @@ public class Testing
         // Assert
         slug.Should().Be("create-a-new-slug-from-english-txt");
     }
+
+    [Fact]
+    public void Space_following_number_slug()
+    {
+        // Arrange
+        var text = "Child page 2";
+
+        // Act
+        var slug = Slug.Create(text);
+
+        // Assert
+        slug.Should().Be("child-page-2");
+    }
 }
